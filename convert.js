35877d2baef43c39e524734bd1af35de3d8f4f56
Junya Ishihara <junya@champierre.com>
2023-01-27 08:24:07 +0900

    Show data on a map.

---
+++ convert.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const datafile = process.argv[2]
+
+const data = JSON.parse(fs.readFileSync(datafile, 'utf8'))
+const features = []
+
+for (const feature of data['features']) {
+  features.push({
+    type: 'Feature',
+    geometry: feature['geometry'],
+    properties: {
+      title: `${feature['properties']['大字名'] !== null ? feature['properties']['大字名'] : ''}${feature['properties']['地番'] !== null ? feature['properties']['地番'] : ''}`
+    }
+  })
+}
+
+fs.writeFileSync('out.geojson', JSON.stringify({
+  type: 'FeatureCollection',
+  features: features
+}))
